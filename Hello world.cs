80a0943f81e93be992cc0a80bf6fa5e553581a58
yan-az <62633804+yan-az@users.noreply.github.com>
2020-05-26 18:19:39 +0900

    Update Hello world.cs

--- Hello world.cs
+++ Hello world.cs
@@ -2,11 +2,11 @@ using system;
 
 namespace test
 {
-	class test
-	{
-       　　static void Main(string[] args)
-	   {
-	Console.WriteLine(Hello World!);
-	   }
-	}
+　　class test
+　　{
+       static void Main(string[] args)
+　　　 {
+	 Console.WriteLine("Hello World!");
+　　　　}
+　　}
 }
